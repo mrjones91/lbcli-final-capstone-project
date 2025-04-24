cccde4bffb7118573cafc10d7317e63ade254b81
DJ <dj@code-crew.org>
2025-04-24 10:57:32 -0500

    1-2

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,2 @@
 # What is the hash of block 243,833?
+bitcoin-cli -regtest getblockhash 243833
--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,12 @@
 # How many new outputs were created by block 243,825?
+sum=0
+txs=$(bitcoin-cli -regtest getblock $(bitcoin-cli -regtest getblockhash 243825) | jq -r '.tx[]')
+
+for tx in $txs
+do
+    # echo $tx
+    count=$(bitcoin-cli -regtest decoderawtransaction $(bitcoin-cli -regtest getrawtransaction $tx) | jq '.vout | length')
+    i=$((count + sum))
+    sum=$i
+done
+echo $sum
