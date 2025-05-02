00d09eabaa1445b55db65dc7448c0f47f6de3b59
DJ <dj@code-crew.org>
2025-05-02 15:51:14 -0500

    1-7

--- .github/setup.sh
+++ .github/setup.sh
@@ -2,8 +2,6 @@ wget https://bitcoincore.org/bin/bitcoin-core-28.0/bitcoin-28.0-x86_64-linux-gnu
 tar -xzvf bitcoin-28.0-x86_64-linux-gnu.tar.gz
 ln -s $PWD/bitcoin-28.0/bin/* /usr/local/bin/
 mkdir -p ~/.bitcoin
-# echo "signet=1" >> ~/.bitcoin/bitcoin.conf
-# echo "[signet]" >> ~/.bitcoin/bitcoin.conf
 echo "rpcconnect=165.22.121.70" >> ~/.bitcoin/bitcoin.conf
 echo "rpcuser=btrustbuildersrpc" >> ~/.bitcoin/bitcoin.conf
 echo "rpcpassword=btrustbuilderspass" >> ~/.bitcoin/bitcoin.conf
